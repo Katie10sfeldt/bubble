144c7cfdb949c21b7adb9d0d3a077bc75183ed3f
Katie Tensfeldt <kmtensfeldt@gmail.com>
2020-12-09 16:31:31 -0800

    Initial app set up

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/App.scss';
+
+import Bubble from './Bubble';
+
+const App = () => {
+	return (
+		<div>
+			<Bubble />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Bubble.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Bubble() {
+	return <div className='bubble bubble__big'>Big Bubble</div>;
+}
